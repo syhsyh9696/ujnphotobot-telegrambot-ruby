7f1b7ec94ef86827148465fd946750f335754728
Yuanhao Sun <sunyuanhao123456@gmail.com>
2017-06-04 22:57:49 +0800

    New stable version, @ujnphotobot in telegram

--- telegram-bot.rb
+++ telegram-bot.rb
@@ -1,108 +1,123 @@
 # encoding: utf-8
 
 require 'telegram/bot'
-require_relative 'Kernel'
+require 'mysql2'
 
 base_url = "http://iplat.ujn.edu.cn/photo/"
 TOKEN = "299913435:AAH0acebPnG2CuaXNU9mv2jwaKgprDbGzuQ"
 
+
+def name_selector(str)
+    client = Mysql2::Client.new(:host => '127.0.0.1',
+                                :username => 'root',
+                                :password => 'XuHefeng',
+                                :database => 'student')
+
+    select_sql = "SELECT stu_num,stu_name FROM info_old WHERE stu_name LIKE \'#{str}\'"
+    result = client.query(select_sql)
+    client.close
+    result
+end
+
 Telegram::Bot::Client.run(TOKEN) do |bot|
-    bot.listen do |message|
-        substr = message.text.split(" ")
-        next if substr[0] == nil
-        case substr[0]
-        when '/start'
-            if message.from.first_name != nil
-                bot.api.send_message(chat_id: message.chat.id, text: "Hello,#{message.from.first_name}")
-            else
-                bot.api.send_message(chat_id: message.chat.id, text: "Hello,welcome to use @ujnphotobot")
-            end
-        when '/stop'
-            if message.from.first_name != nil
-                bot.api.send_message(chat_id: message.chat.id, text: "Bye,#{message.from.first_name}")
-            else
-                bot.api.send_message(chat_id: message.chat.id, text: "Bye,maybe see you next time")
-            end
-        when '/random'
-            line = Unicorn::randomnum()
-            fn = "#{line[0].to_s}.jpg"
-            url = base_url + "#{line[0][1..4]}/" + fn[1..15]
-            bot.api.send_chat_action(chat_id: message.chat.id, action: "upload_photo")
-            bot.api.send_photo(chat_id: message.chat.id, photo: "#{url}", caption: "#{line[0]}>>#{line[1]}>>#{line[2]}>>#{line[3]}")
-        when '/random@ujnphotobot'
-            line = Unicorn::randomnum()
-            fn = "#{line[0].to_s}.jpg"
-            url = base_url + "#{line[0][1..4]}/" + fn[1..15]
-            #bot.api.send_message(chat_id: message.chat.id, text: "#{line}")
-            bot.api.send_chat_action(chat_id: message.chat.id, action: "upload_photo")
-            bot.api.send_photo(chat_id: message.chat.id, photo: "#{url}", caption: "#{line[0]}>>#{line[1]}>>#{line[2]}>>#{line[3]}")
-        when '/randomgirl'
-            line = Unicorn::randomgirl()
-            fn = "#{line.to_s}.jpg"
-            url = base_url + "#{line[1..4]}/" + fn[1..15]
-            bot.api.send_chat_action(chat_id: message.chat.id, action: "upload_photo")
-            bot.api.send_photo(chat_id: message.chat.id, photo: "#{url}")
-        when '/randomgirl@ujnphotobot'
-            line = Unicorn::randomgirl()
-            fn = "#{line.to_s}.jpg"
-            url = base_url + "#{line[1..4]}/" + fn[1..15]
-            bot.api.send_chat_action(chat_id: message.chat.id, action: "upload_photo")
-            bot.api.send_photo(chat_id: message.chat.id, photo: "#{url}")
-        when '/num'
-            if substr[1] == nil
-                bot.api.send_chat_action(chat_id: message.chat.id, action: "upload_photo")
-                bot.api.send_message(chat_id: message.chat.id, text: "Can't find the student number")
-                next
-            end
-            if Unicorn::checknumber(substr[1])
-                stunum = substr[1].to_s
-                url = base_url + "#{stunum[0..3]}/#{stunum}.jpg"
-                bot.api.send_chat_action(chat_id: message.chat.id, action: "upload_photo")
-                bot.api.send_photo(chat_id: message.chat.id, photo: "#{url}")
-                Unicorn::numberlog(substr[1], message.from.username, message.from.first_name, message.from.last_name)
-            else
-                bot.api.send_chat_action(chat_id: message.chat.id, action: "typing")
-                bot.api.send_message(chat_id: message.chat.id, text: "Can't find the student number")
-                next
-            end
-        when '/help'
-            bot.api.send_message(chat_id: message.chat.id, text: "#{Unicorn::help()}")
-        when '/doge'
-            url = base_url + "2014/20141222216.jpg"
-            bot.api.send_photo(chat_id: message.chat.id, photo: "#{url}")
-        when '/ha'
-            bot.api.send_chat_action(chat_id: message.chat.id, action: "typing")
-            bot.api.send_sticker(chat_id: message.chat.id, sticker: "BQADBQADBgADqno2BgFR4Dylw9eQAg")
-            bot.api.send_message(chat_id: message.chat.id, text: "不要总想搞个大新闻")
-        when '/ha@ujnphotobot'
-            bot.api.send_chat_action(chat_id: message.chat.id, action: "typing")
-            bot.api.send_sticker(chat_id: message.chat.id, sticker: "BQADBQADBgADqno2BgFR4Dylw9eQAg")
-            bot.api.send_message(chat_id: message.chat.id, text: "不要总想搞个大新闻")
-        when '/name'
-            if substr[1] == nil
-                bot.api.send_message(chat_id: message.chat.id, text: "Can't find the student name")
-                next
-            end
-            line = Unicorn::checkname(substr[1])
-            if line == nil
-                bot.api.send_message(chat_id: message.chat.id, text: "Can't find the student name")
-            elsif line.size == 1
-                fn = "#{line[0][0].to_s}.jpg"
-                url = base_url + "#{line[0][0][1..4]}/" + fn[1..15]
-                bot.api.send_chat_action(chat_id: message.chat.id, action: "upload_photo")
-                bot.api.send_photo(chat_id: message.chat.id, photo: "#{url}")
-                Unicorn::namelog(substr[1], message.from.username, message.from.first_name, message.from.last_name)
-            elsif line.size > 1
-                bot.api.send_chat_action(chat_id: message.chat.id, action: "typing")
-                bot.api.send_message(chat_id: message.chat.id, text: "一共有#{line.size}个名字为<<#{substr[1]}>>的人")
-                line.size.times do |i|
-                    fn = "#{line[i][0].to_s}.jpg"
-                    url = base_url + "#{line[i][0][1..4]}/" + fn[1..15]
+    handle_thread = Thread.new do
+        bot.listen do |message|
+            begin
+                substr = message.text.split(" ")
+                next if substr[0] == nil
+                command = substr[0].upcase
+                case command
+                when '/START'
+                    if message.from.first_name != nil
+                        bot.api.send_message(chat_id: message.chat.id, text: "Hello,#{message.from.first_name}SiJi")
+                    else
+                        bot.api.send_message(chat_id: message.chat.id, text: "Hello,welcome to use @ujnlaosijibot")
+                    end
+                when '/STOP'
+                    if message.from.first_name != nil
+                        bot.api.send_message(chat_id: message.chat.id, text: "Bye,#{message.from.first_name}")
+                    else
+                        bot.api.send_message(chat_id: message.chat.id, text: "Bye,maybe see you next time")
+                    end
+                when '/NAME'
+                    if substr[1] == nil
+                        bot.api.send_message(chat_id: message.chat.id, text: "你输入的Name是tan90")
+                        next
+                    end
+                    result = name_selector(substr[1])
+                    if result == nil
+                        bot.api.send_message(chat_id: message.chat.id, text: "你输入的ID是tan90")
+                        next
+                    end
+                    result.each do |elements|
+                        e = elements["stu_num"]
+                        t = elements["stu_name"]
+                        bot.api.send_chat_action(chat_id: message.chat.id, action: "upload_photo")
+                        bot.api.send_photo(chat_id: message.chat.id,
+                            photo: "iplat.ujn.edu.cn/photo/#{e[1..4]}/#{e[1..-1]}.jpg",
+                            caption: "#{t}")
+                    end
+                when '/NUM'
+                    if substr[1] == nil
+                        bot.api.send_message(chat_id: message.chat.id, text: "你输入的ID是tan90")
+                        next
+                    end
+                    e = substr[1]
+                    if e == "220141222124" || e == "20141222124"
+                        bot.api.send_chat_action(chat_id: message.chat.id, action: "upload_photo")
+                        bot.api.send_photo(chat_id: message.chat.id,
+                            photo: "http://iplat.ujn.edu.cn/photo/2014/20141222100.jpg")
+                        next
+                    end
+
                     bot.api.send_chat_action(chat_id: message.chat.id, action: "upload_photo")
-                    bot.api.send_photo(chat_id: message.chat.id, photo: "#{url}")
+                    bot.api.send_photo(chat_id: message.chat.id, photo: "http://iplat.ujn.edu.cn/photo/#{e[1..4]}/#{e[1..-1]}.jpg") if e.size == 12
+                    bot.api.send_photo(chat_id: message.chat.id, photo: "http://iplat.ujn.edu.cn/photo/#{e[0..3]}/#{e[0..-1]}.jpg") if e.size == 11
+                when '/NAME@UJNPHOTOBOT'
+                    if substr[1] == nil
+                        bot.api.send_message(chat_id: message.chat.id, text: "你输入的Name是tan90")
+                        next
+                    end
+                    result = name_selector(substr[1])
+                    if result == nil
+                        bot.api.send_message(chat_id: message.chat.id, text: "你输入的ID是tan90")
+                        next
+                    end
+                    result.each do |elements|
+                        e = elements["stu_num"]
+                        t = elements["stu_name"]
+                        bot.api.send_chat_action(chat_id: message.chat.id, action: "upload_photo")
+                        bot.api.send_photo(chat_id: message.chat.id,
+                            photo: "iplat.ujn.edu.cn/photo/#{e[1..4]}/#{e[1..-1]}.jpg",
+                            caption: "#{t}")
+                    end
+                when '/NUM@UJNPHOTOBOT'
+                    if substr[1] == nil
+                        bot.api.send_message(chat_id: message.chat.id, text: "你输入的ID是tan90")
+                        next
+                    end
+                    e = substr[1]
+                    if e == "220141222124" || e == "20141222124"
+                        bot.api.send_chat_action(chat_id: message.chat.id, action: "upload_photo")
+                        bot.api.send_photo(chat_id: message.chat.id,
+                            photo: "http://iplat.ujn.edu.cn/photo/2014/20141222100.jpg")
+                        next
+                    end
+
+                    bot.api.send_chat_action(chat_id: message.chat.id, action: "upload_photo")
+                    bot.api.send_photo(chat_id: message.chat.id, photo: "http://iplat.ujn.edu.cn/photo/#{e[1..4]}/#{e[1..-1]}.jpg") if e.size == 12
+                    bot.api.send_photo(chat_id: message.chat.id, photo: "http://iplat.ujn.edu.cn/photo/#{e[0..3]}/#{e[0..-1]}.jpg") if e.size == 11
                 end
-                Unicorn::namelog(substr[1], message.from.username, message.from.first_name, message.from.last_name)
+            rescue Exception => e
+                next
             end
         end
+    end
+
+    begin
+        handle_thread.join
+    rescue
+        GC.start
+        retry
     end
 end
